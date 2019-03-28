27a04b07624d3d2133c84b5e615f1fcf00ec3343
Asuna <chuxubank@qq.com>
2019-03-28 11:34:27 +0800

    add footnote

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,24 +7,25 @@ module.exports = {
         repo: 'chuxubank/Learning-iOS',
         editLinks: true,
         nav: [{
-                text: 'Home',
-                link: '/'
-            },
-            {
-                text: 'Stanford',
+            text: 'Home',
+            link: '/'
+        },
+        {
+            text: 'Stanford',
+            items: [{
+                text: 'CS193P iPhone Application Development',
                 items: [{
-                    text: 'CS193P iPhone Application Development',
-                    items: [{
-                        text: 'Developing iOS 11 Apps with Swift',
-                        link: '/Developing-iOS-11-Apps-with-Swift/'
-                    }]
+                    text: 'Developing iOS 11 Apps with Swift',
+                    link: '/Developing-iOS-11-Apps-with-Swift/'
                 }]
-            }
+            }]
+        }
         ],
         sidebar: {
             '/Developing-iOS-11-Apps-with-Swift/': [
                 'L1-Introduction-to-iOS11-Xcode9-Swift4/',
-                'L2-MVC-iOS-Xcode-Swift-Demonstration/'
+                'L2-MVC-iOS-Xcode-Swift-Demonstration/',
+                'L3-Swift-Overview/'
             ]
         }
     },
@@ -40,6 +41,7 @@ module.exports = {
             md.use(require('markdown-it-checkbox'))
             md.use(require('markdown-it-plantuml'))
             md.use(require('markdown-it-mermaid').default)
+            md.use(require('markdown-it-footnote'))
         }
     }
 }
